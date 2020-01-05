2e900b6eb41fd1c2b58d908b804638842a0333a7
Albert van Kiel <albert@albert-inspiron-5737.home>
2020-01-05 20:08:17 +0100

    login screen

---
+++ backend/resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.master')
+
+@section('title', 'Login page')
+
+@yield('sidebar')
+
+@section('content')
+    This is the dashboard
+@endsection
--- backend/resources/views/login.blade.php
+++ backend/resources/views/login.blade.php
@@ -15,25 +15,14 @@
         </div>
         <div class="col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r" style='min-width: 320px;'>
             <h4 class="fw-300 c-grey-900 mB-40">Login</h4>
+            <br>
             <form>
                 <div class="form-group">
-                    <label class="text-normal text-dark">Username</label>
-                    <input type="email" class="form-control" placeholder="John Doe">
-                </div>
-                <div class="form-group">
-                    <label class="text-normal text-dark">Password</label>
-                    <input type="password" class="form-control" placeholder="Password">
+                    <label class="text-normal text-dark">Pin number</label>
+                    <input type="pin" class="form-control" placeholder="1234">
                 </div>
                 <div class="form-group">
                     <div class="peers ai-c jc-sb fxw-nw">
-                        <div class="peer">
-                            <div class="checkbox checkbox-circle checkbox-info peers ai-c">
-                                <input type="checkbox" id="inputCall1" name="inputCheckboxesCall" class="peer">
-                                <label for="inputCall1" class=" peers peer-greed js-sb ai-c">
-                                    <span class="peer peer-greed">Remember Me</span>
-                                </label>
-                            </div>
-                        </div>
                         <div class="peer">
                             <button class="btn btn-primary">Login</button>
                         </div>
